b635cf38cc49642f6b72fce8a6fe6ee24404137c
Eduardo Patrocinio <eduardop@us.ibm.com>
2019-03-19 15:26:17 -0400

    Delete old role binding

--- Game-1/createRoleBinding.sh
+++ Game-1/createRoleBinding.sh
@@ -1,5 +1,7 @@
 NAMESPACE=ping-pong
 
+kubectl delete rolebinding pp-rolebinding
+
 kubectl create rolebinding -n $NAMESPACE pp-rolebinding --clusterrole=ibm-privileged-clusterrole \
 	--serviceaccount=$NAMESPACE:default
 
