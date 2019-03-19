ee9cee05537222a18e3201e6b25f9c6bebff4c5e
Eduardo Patrocinio <eduardop@us.ibm.com>
2019-03-19 14:40:44 -0400

    Fix PSP problem

--- Game-1/createRoleBinding.sh
+++ Game-1/createRoleBinding.sh
@@ -2,3 +2,7 @@ NAMESPACE=ping-pong
 
 kubectl create rolebinding -n $NAMESPACE pp-rolebinding --clusterrole=privileged \
 	--serviceaccount=$NAMESPACE:default
+
+kubectl delete psp retrict-root
+
+kubectl create -f permit_root.yaml 
